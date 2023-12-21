7b0c3642bc36be906798b6b48edeb898145050aa
Tjeerd <tjeerdweijers@gmail.com>
2023-12-21 21:17:55 +0100

    small changes

--- src/miditools.cpp
+++ src/miditools.cpp
@@ -1,24 +1,36 @@
-#include "Arduino.h"
 #include "miditools.h"
 
-notesMemory::notesMemory(int channel, MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial>>& midiOutPort)
-    : _channel(channel), _midiOutPort(midiOutPort)
+notesMemory::notesMemory( MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial>>& midiOutPort)
+    :  _midiOutPort(midiOutPort)
 {
-    noteMemA = 0;
-    noteMemB = 0;
-    noteMemC = 0;
-    noteMemD = 0;
+    //noteMemA = 0;
+    //noteMemB = 0;
+    //noteMemC = 0;
+    //noteMemD = 0;
 }
 
 
-bool notesMemory::isAnythingOn(){
-    if(noteMemA==0 && noteMemB==0 && noteMemC==0 && noteMemD==0){
+bool notesMemory::isAnythingOn(int channel){
+    if(noteMemA[channel]==0 && noteMemB[channel]==0 && noteMemC[channel]==0 && noteMemD[channel]==0){
         return false;
     }
     else{
         return true;
     }
 }
-void notesMemory::turnNotesOff(){
+
+void notesMemory::koppelOnNotes(int sourceChannel, int DestinationChannel){
+    //go through source channelmem and send note on to dest channel
+    //also send to memory
 }
+
+void notesMemory::koppelOffNotes(int Destinationchannel){
+    //
+}
+
+void notesMemory::putNoteInMemory(int channel, int note, bool fromKoppel, bool noteOn){
+
+}
+
+
 
