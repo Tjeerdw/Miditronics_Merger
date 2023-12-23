fd935c413a967de4cbe826df9d5ab4246a9aa7a5
Tjeerd <tjeerdweijers@gmail.com>
2023-12-23 15:10:43 +0100

    some pseudocode

--- src/miditools.cpp
+++ src/miditools.cpp
@@ -2,6 +2,9 @@
 
 #define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
 #define koppelsChannel 10
+#define registerChannelA 8
+#define registerChannelB 9
+
 
 KoppelUnit::KoppelUnit( MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial>>& midiOutPort)
     :  _midiOutPort(midiOutPort)
@@ -15,6 +18,31 @@ void KoppelUnit::handleKoppels(midi::MidiType type,  midi::Channel channel, byte
     #ifndef useUSBMIDI
     Serial.printf("type: %d  Channel %d  data1 %d  data2 %d\n",type,channel,data1,data2);
     #endif
+    
+    if (type == midi::NoteOff ||type == midi::NoteOn){ //note message incoming
+        /*
+        go through koppelList, find matching source channel and enabled
+            if note on
+                if note in desination mem is off
+                    send note to desitnation channel 
+                else
+                    don'tsend note
+                edit note in destination notesmem
+                
+             if note off
+                edit note in destination notesmem
+                if deistation mem=0
+                    send note off
+                */
+    }
+    else if (type == midi::ControlChange && channel == koppelsChannel){ //koppel message incoming
+    
+    }
+    else if ((type == midi::ControlChange && channel == (registerChannelA || registerChannelB)){ //register message incoming
+    
+    }
+    
+    
     if (type == midi::NoteOff ||type == midi::NoteOn){ // if notes, check if there is a matching couple that is enabled
         for (int i=0 ; i<g_KoppelRows;i++){
             //Serial.printf("%d,%d,%d,%d\n",koppels[i][0],koppels[i][1],koppels[i][2],koppels[i][3]); //print out koppel array
