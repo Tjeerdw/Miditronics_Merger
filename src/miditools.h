6f0d5348828ebaccd91526edc34f70d4fbd413ac
Tjeerdw <tjeerdweijers@gmail.com>
2025-03-31 21:37:07 +0200

    added 3 octave couplings in KL

--- src/miditools.h
+++ src/miditools.h
@@ -18,6 +18,7 @@
 #define KL_Source 2
 #define KL_Destination 3
 #define KL_KoppelBit 4
+#define KL_Transpose
 
 class KoppelUnit{
     public:
@@ -31,30 +32,33 @@ class KoppelUnit{
         MIDI_NAMESPACE::MidiInterface<MIDI_NAMESPACE::SerialMIDI<HardwareSerial>>& _midiOutPort;
         uint32_t notesMem [7][128]; //6 organs, 128 notes each, 32 bits for sources each, organ 0 not used
         bool reactToKoppels;
-        int koppelList[21][5] =
+        int koppelList[24][6] =
         {
             // Midi Value, enabled, note source ch, note destination ch, noteMembit
-            {58,1,1,1,1}, // I+I
-            {24,0,6,1,2}, // P+I
-            {59,1,2,2,1}, // II+II
-            {1 ,0,1,2,2}, // I+II
-            {25,0,6,2,3}, // P+II  
-            {60,1,3,3,1}, // III+III
-            {2 ,0,1,3,2}, // I+III
-            {7 ,0,2,3,3}, // II+III
-            {26,0,6,3,4}, // P+III
-            {61,1,4,4,1}, // IV+IV
-            {3 ,0,1,4,2}, // I+IV
-            {8 ,0,2,4,3}, // II+IV
-            {11,0,3,4,4}, // III+IV
-            {27,0,6,4,5}, // P+IV
-            {62,1,5,5,1}, // V+V
-            {20,0,1,5,2}, // I+V
-            {21,0,2,5,3}, // II+V
-            {22,0,3,5,4}, // III+V
-            {29,0,4,5,5}, // IV+V
-            {28,0,6,5,6}, // P+V 
-            {63,1,6,6,1}, // P+P
+            {58,1,1,1,1,0},   // I+I
+            {24,0,6,1,2,0},   // P+I
+            {59,1,2,2,1,0},   // II+II
+            {1 ,0,1,2,2,0},   // I+II
+            {4 ,0,1,2,2,-12}, // I+II 16'
+            {25,0,6,2,3,0},   // P+II  
+            {60,1,3,3,1,0},   // III+III
+            {2 ,0,1,3,2,0},   // I+III
+            {6 ,0,1,3,2,-12},   // I+III 16'
+            {7 ,0,2,3,3,0},   // II+III
+            {9 ,0,2,3,3,-12},   // II+III 16'
+            {26,0,6,3,4,0},   // P+III
+            {61,1,4,4,1,0},   // IV+IV
+            {3 ,0,1,4,2,0},   // I+IV
+            {8 ,0,2,4,3,0},   // II+IV
+            {11,0,3,4,4,0},   // III+IV
+            {27,0,6,4,5,0},   // P+IV
+            {62,1,5,5,1,0},   // V+V
+            {20,0,1,5,2,0},   // I+V
+            {21,0,2,5,3,0},   // II+V
+            {22,0,3,5,4,0},   // III+V
+            {29,0,4,5,5,0},   // IV+V
+            {28,0,6,5,6,0},   // P+V 
+            {63,1,6,6,1,0},   // P+P
         };
         int g_KoppelRows;
         int g_NumberOfNotes;
