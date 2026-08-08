901baab81bfaf8bc06ef43ff6da145edf95e7ee8
Tjeerdw <tjeerdweijers@gmail.com>
2026-08-08 20:30:55 +0200

    fixed CC81 double-up tranpose bug

--- src/miditools.cpp
+++ src/miditools.cpp
@@ -48,7 +48,7 @@ void KoppelUnit::handleKoppels(midi::MidiType type,  midi::Channel channel, byte
                         koppelList[i][KL_Enabled] = 0; //disable matching koppel in koppel mem
                         for ( int j=0 ; j<g_NumberOfNotes ; j++){ //go though all notes in destination channel
                             if (notesMem[KL_destinationChannel][j] & 1<<KL_koppelbitje){  //search for koppelbits on in destination channel "staat er iets aan dat uit moet
-                                koppelNoteOff(midi::NoteOff,j,data2,KL_destinationChannel, sendToUSB, i,KL_transpose); 
+                                koppelNoteOff(midi::NoteOff,j,data2,KL_destinationChannel, sendToUSB, i,0); //no need to transpose since it allready has been.
                             }
                         }
                         break;
